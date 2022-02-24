a6b64de96e70878215ce4aa8f645160f9d919446
Erick <gonzalez20157@yahoo.com>
2022-02-24 09:25:05 -0600

    commit tree a3a6e960bec3f2be85a89f75efd7d98516b85313

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+let num1 = 2;
+let num2 = 31;
+let multiply = num1 * num2;
 
-// Write your code here
+//console.log(multiply (2,31))
+let random = Math.floor(Math.random()*10);
+
+let num3 = 44 
+let num4 = 10 
+let mod = num3 % num4 
+
+let max = Math.max(1,2,4,20,8);
